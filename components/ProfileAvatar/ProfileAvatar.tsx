d1fcc23a5ad2d7a45648f9d63b12680d21d548ca
Kravets <ihor.kravets@leasoft.org>
2026-01-10 18:27:04 +0100

    Add ProfileAvatar component with styles and image
    Implemented the ProfileAvatar component with layout, styles, and a sample avatar image. The component displays a user's avatar, name, email, and a button to upload a new photo. Added corresponding CSS module for styling and included a default avatar image.

--- components/ProfileAvatar/ProfileAvatar.tsx
+++ components/ProfileAvatar/ProfileAvatar.tsx
@@ -1,3 +1,25 @@
+import css from './ProfileAvatar.module.css';
+import Image from 'next/image';
+
 export default function ProfileAvatar() {
-  return <div></div>;
+  return (
+    <div className={css.container}>
+      <Image
+        src="/img/avatar.jpg"
+        width={132}
+        height={132}
+        className={css.avatar}
+        alt="User's avatar"
+      />
+      <div className={css.info}>
+        <div className={css.details}>
+            <h3 className={css.name}>Ганна</h3>
+            <p className={css.email}>hanna@gmail.com</p>
+        </div>
+        <button type="button" className={css.button}>
+          Завантажити нове фото
+        </button>
+      </div>
+    </div>
+  );
 }
