04754691a7567e655f73be947ce0a3abd0db9df2
Mykhailo Zanizdra <yamixa.gz@gmail.com>
2026-01-14 23:56:33 +0200

    fix: Fixed titles text owerflowing.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import './reset.css';
 import './globals.css';
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 import { lato, comfortaa } from './fonts';
