10b832586bd115098f51f92405fdcd02741fb445
Mykhailo Zanizdra <yamixa.gz@gmail.com>
2026-01-15 15:34:53 +0200

    feat: Added metategs.

--- app/(privat routes)/diary/page.tsx
+++ app/(privat routes)/diary/page.tsx
@@ -3,6 +3,12 @@ import { fetchServerDiaries } from '@/lib/api/serverApi';
 import { HydrationBoundary } from '@tanstack/react-query';
 import { dehydrate } from '@tanstack/react-query';
 import DiaryPageClient from './DiaryPage.client';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Щоденник',
+  description: 'Проглядайте та редагуйте записи у щоденнику',
+};
 
 export default async function DiaryPage() {
   const queryClient = new QueryClient();
