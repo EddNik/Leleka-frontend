c4f829269a3625c2cf63f13088c993abc74ec004
Mykhailo Zanizdra <yamixa.gz@gmail.com>
2026-01-18 11:53:57 +0200

    fix: Fixed button styles

--- components/AuthBar/AuthBar.tsx
+++ components/AuthBar/AuthBar.tsx
@@ -6,13 +6,16 @@ export default function AuthBar() {
   return (
     <div className={styles.boxFooter}>
       <ul className={styles.boxFooterList}>
-        <li className={clsx(styles.sign_link_modal)}>
+        <li className={styles.sign_link_modal}>
           <Link className={styles.sign_link} href="/sign-up">
             Зареєстуватись
           </Link>
         </li>
-        <li className={clsx(styles.sign_link_modal)}>
-          <Link className={styles.sign_link} href="/sign-in">
+        <li className={styles.sign_link_modal}>
+          <Link
+            className={clsx(styles.sign_link, styles.sign_link_secondary)}
+            href="/sign-in"
+          >
             Увійти
           </Link>
         </li>
